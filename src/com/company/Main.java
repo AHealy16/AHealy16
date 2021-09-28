889f7523d15eca2c1d51dd829f794a67b9f74c71
AHealy16 <AH211841@reigate.ac.uk>
2021-09-28 16:13:40 +0100

    ...

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] userNum = {0,0,0,0,0};
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 5; i++);
+
+
+
+
+    }
+}
